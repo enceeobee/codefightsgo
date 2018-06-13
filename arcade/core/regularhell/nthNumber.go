d342e4a771f0700b8a7e915cde8cb9c5a705e81e
Nate Bisbee <ncob@users.noreply.github.com>
2018-06-12 22:02:48 -0400

    Solve nthNumber

--- arcade/core/regularhell/nthNumber.go
+++ arcade/core/regularhell/nthNumber.go
@@ -6,6 +6,6 @@ import (
 )
 
 func nthNumber(s string, n int) string {
-	re := regexp.MustCompile("(?:(?:\\d[a-zA-Z0-9]*).){" + strconv.Itoa(n-1) + "}\\D*0*(\\d*)")
+	re := regexp.MustCompile("(?:\\d+\\D+){" + strconv.Itoa(n-1) + "}\\D*0*(\\d*)")
 	return re.FindStringSubmatch(s)[1]
 }
